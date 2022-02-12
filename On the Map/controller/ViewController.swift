a98b48b16be1e02bae9244fadf6a498e9ab7a60e
Marcos <mmejiasg@zoho.com>
2022-02-12 11:44:59 +0100

    Initial code

---
+++ On the Map/controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by Marcos Mejias on 12/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
